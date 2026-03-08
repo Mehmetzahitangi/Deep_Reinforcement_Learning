6fccf7991a35bee8e7ea553e71a4a36b26fd34c8
mehmetzahitangi <mehmetzahitangi@gmail.com>
2026-03-08 17:47:58 +0300

    Add inference code and trained data

--- Chapter 6 Deep Q-Networks/03_DQN_play.py
+++ Chapter 6 Deep Q-Networks/03_DQN_play.py
@@ -15,7 +15,8 @@ DEFAULT_ENV_NAME = "ALE/Pong-v5"
 FPS = 25
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
+    """--- INFERENCE ---"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model", required=True,
                         help="Model file to load")
@@ -30,13 +31,13 @@ if __name__ == "__main__":
 
     env = wrappers.make_env(args.env)
     if args.record:
-        env = gym.wrappers.Monitor(env, args.record)
+        env = gym.wrappers.RecordVideo(env, video_folder=args.record)
     net = dqn_model.DQN(env.observation_space.shape,
                         env.action_space.n)
     state = torch.load(args.model, map_location=lambda stg, _: stg)
     net.load_state_dict(state)
 
-    state = env.reset()
+    state, _ = env.reset()
     total_reward = 0.0
     c = collections.Counter()
 
@@ -44,18 +45,19 @@ if __name__ == "__main__":
         start_ts = time.time()
         if args.vis:
             env.render()
-        state_v = torch.tensor(np.array([state], copy=False))
-        q_vals = net(state_v).data.numpy()[0]
-        action = np.argmax(q_vals)
-        c[action] += 1
-        state, reward, done, _ = env.step(action)
+        state_v = torch.tensor(np.array([state])) # O anki state'i tensor'a çevirip ağa veririz.
+        q_vals = net(state_v).data.numpy()[0] # Ağ, olası 6 tuş için 6 farklı Q-değeri döndürür
+        action = np.argmax(q_vals) # Değerler içindeki en yüksek olanı seçeriz
+        c[action] += 1 # Oyun bittiğinde ajanın hangi tuşlara ne kadar bastığını gösterir. Eğitimsiz bir ajan genelde takılı kalıp hep aynı tuşa basar. Eğitimli bir ajan ise tuşları duruma göre dengeli kullanır.
+        state, reward, terminated, truncated , _ = env.step(action)
+        done = terminated or truncated
         total_reward += reward
         if done:
             break
         if args.vis:
             delta = 1/FPS - (time.time() - start_ts)
             if delta > 0:
-                time.sleep(delta)
+                time.sleep(delta) # Ajan oyunu saniyede binlerce kare hızında oynayabilir. Eğer oyunu ekranda izlemek (--vis) istersek, çok hızlı akıp bitmemesi için saniyede 25 kareye (FPS) sabitleriz.
     print("Total reward: %.2f" % total_reward)
     print("Action counts:", c)
     if args.record:
